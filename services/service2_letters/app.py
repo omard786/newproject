54a48e7dc958b549bbf7d586de1dd9f25805ec11
omard786 <omardhin@gmail.com>
2021-04-05 13:20:58 +0000

    adding the flask app

--- services/service2_letters/app.py
+++ services/service2_letters/app.py
@@ -1,20 +1,31 @@
 import random 
+from flask import Flask
 
-letter_list = ['A','E','I','U','B','F','J','V']
-vowels = 'AEIU'
-consonant = 'BFJV'
-#choices let you pick the letters from the lise, k=amount of objects from the list, weights is the prob of occuring  
-letter= random.choices(letter_list, weights=[1, 1, 1, 1, 1, 1, 1, 1], k=4)
-
-print('these are your letters :')
-dash_list= print('-'.join(letter))
-
-#sort it so the letters dont reproduce 
+app = Flask(__name__)
 
 
-#list comphrension, using one list with another 
-vowel_count = [i for i in letter if i in vowels]
-print ('you have this many vowels : ', len(vowel_count))
 
-consonant_count = [i for i in letter if i in consonant]
-print ('you have this many consonants : ', len(consonant_count))
+@app.route("/letter_gen")
+def letter_gen():
+    letter_list = ['A','E','I','U','B','F','J','V']
+    vowels = 'AEIU'
+    consonant = 'BFJV'
+    #choices let you pick the letters from the lise, k=amount of objects from the list, weights is the prob of occuring  
+    letter= random.choices(letter_list, weights=[1, 1, 1, 1, 1, 1, 1, 1], k=4)
+
+    #puts dash (-) in list to separate 
+    dash_list='-'.join(letter)
+
+    #sort it so the letters dont reproduce 
+
+
+    #list comphrension, using one list with another 
+    vowel_count = [i for i in letter if i in vowels]
+    consonant_count = [i for i in letter if i in consonant]
+    return f"{len(vowel_count)} {len(consonant_count)} {dash_list}"
+    
+
+
+if __name__ == '__main__':
+    app.run(host = "0.0.0.0", port = 5002, debug= True )
+
