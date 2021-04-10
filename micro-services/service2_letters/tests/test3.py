4cb24094bdb886aebc626c48115b9ea7b33a764b
omard786 <omardhin@gmail.com>
2021-04-10 15:14:39 +0000

    confirguring jenkinsfile

---
+++ micro-services/service2_letters/tests/test3.py
@@ -0,0 +1,21 @@
+from app import app
+from unittest.mock import patch
+from flask import url_for
+from flask_testing import TestCase
+import random
+
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestNumbers(TestBase):
+    def test_numbers(self):
+        response = self.client.get(url_for('numbers'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_letters(self):         
+        with patch('random.randint') as a:
+            a.return_value = '3'
+            response = self.client.get(url_for('numbers'))
+            self.assertIn(b'333333', response.data)
--- micro-services/test.sh
+++ micro-services/test.sh
@@ -1,5 +1,13 @@
-#! /bin/bash
-sudo apt-get install python3-venv
+
+#! /bin/bash 
+sudo apt-get install python3-venv -y
 python3 -m venv venv
 source venv/bin/activate
-pip3 install - r requirements.txt
+pip3 install -r requirements.txt
+pip3 install pytest
+pip3 install pytest-cov
+
+python3 -m pytest service_one --cov=service_one --cov-report=term-missing 
+python3 -m pytest service_two --cov=service_two --cov-report=term-missing 
+python3 -m pytest service_three --cov=service_three --cov-report=term-missing 
+python3 -m pytest service_four --cov=service_four --cov-report=term-missing 
