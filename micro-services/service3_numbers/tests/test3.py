db24cd66c165385b7a12da5597f665e6202a378f
omard786 <omardhin@gmail.com>
2021-04-10 18:08:03 +0000

    trying to sort out ansible

--- micro-services/service3_numbers/tests/test3.py
+++ micro-services/service3_numbers/tests/test3.py
@@ -9,13 +9,13 @@ class TestBase(TestCase):
     def create_app(self):
         return app
 
-class TestNumbers(TestBase):
+class testnumbers(TestBase):
     def test_numbers(self):
-        response = self.client.get(url_for('number_gen'))
+        response = self.client.get(url_for('numbers'))
         self.assertEqual(response.status_code, 200)
 
     def test_letters(self):         
         with patch('random.randint') as a:
-            a.return_value = '3'
-            response = self.client.get(url_for('number_gen'))
+            a.return_value = '5'
+            response = self.client.get(url_for('numbers'))
             self.assertIn(b'333333', response.data)
