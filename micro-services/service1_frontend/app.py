3fa3299e7d4d2a25a63c561b17a333259c5ad2e7
omard786 <omardhin@gmail.com>
2021-04-10 14:14:18 +0000

    setting up ansible

--- micro-services/service1_frontend/app.py
+++ micro-services/service1_frontend/app.py
@@ -21,7 +21,7 @@ class prize(db.Model):
 def home():
     s2_letter = requests.get('http://service2_letters:5002/letters').text
     s3_number = requests.get('http://service3_numbers:5003/numbers').text
-    s4_comb = requests.post('http://service4_comb:5004/generator',json={"service2_letter":s2_letter,"service3_numbers":s3_number}).json()
+    s4_comb = requests.post('http://service4_comb:5004/generator',json={"service2_letter":s2_letter,"service3_numbers":s3_number}.json()
     prize_given = prize(number_generated= s4_comb["combined_list"], prize_won = s4_comb["prize"])
     db.session.add(prize_given)
     db.session.commit()
