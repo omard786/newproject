40ff5bb66737337546efaac26e6d04727b6c2655
omard786 <omardhin@gmail.com>
2021-04-11 18:07:08 +0000

    remove ignore files and resubmit

--- micro-services/service4_comb/app.py
+++ micro-services/service4_comb/app.py
@@ -1,13 +1,8 @@
 from flask import Flask, Response, jsonify, json
 import requests
 import string 
-# from flask_sqlalchemy import SQLALchemy
-# from flask_wtf import FlaskForm
 
 app = Flask(__name__)
-
-
-
 
 @app.route('/generator', methods=['GET', 'POST'])
 def generator():
@@ -45,50 +40,6 @@ def generator():
     return jsonify({
        "combined_list":combined_list, "vowel_count":vowel_count, "even_count":even_count, "odd_count": odd_count, "prize":prize,  
     })
-
-    # return packet
-
-
-
-#     if letter_list["vowel_count"] >=2 and number_list["even_count"] >=2:
-#         print ("you have won a gold prize")
-
-#     return jsonify(
-#         {
-#          "letter_list":letter_list, "number_list":number_list
-#         }
-#     )
-
-# prize_tariff = {
-#     "gold": len(even_count) >=2 and len(vowel_count) >=2, 
-#     "silver": len()
-# }
-
-# j_letter= requests.get('http://localhost:5002/letter')
-# data = json.loads(j_letter)
-# print(data)
-# j_letter= requests.get('http://localhost:5002/letter')
-# j_number= requests.get('http://localhost:5003/number')
-# print(j_letter.content)
-# print(j_number.content)
-
-# @app.route("/service2", methods=['GET']) 
-# def service2():
-#     let_list = requests.get('http://service2_letters:5002/letter').text
-#     return f'{let_list} '
-
-
-# @app.route("/service3", methods=['GET']) 
-# def service3():
-#     r_get=requests.get 
-#     number_list=r_get('http://service3_numbers:5003/number').text
-#     return f'{num_list} '
-
-# @app.route('/frontend')
-# def frontend():
-#     service2= service2()
-#     service3 = service3()
-#     return 
 
 
 if __name__ == "__main__":
