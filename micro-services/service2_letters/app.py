4c01048fbee826821ca63c2ae657747ed40817e1
omard786 <omardhin@gmail.com>
2021-04-12 08:43:22 +0000

    last commit ?

--- micro-services/service2_letters/app.py
+++ micro-services/service2_letters/app.py
@@ -11,28 +11,6 @@ def letter_gen():
     random_string = "".join(random.choice(letter_list) for i in range(4))
     return random_string
 
-
-
-
-
-#     letter_list = ['A','E','I','U','B','F','J','V']
-#     vowels = 'AEIU'
-#     consonant = 'BFJV'
-#     #choices let you pick the letters from the lise, k=amount of objects from the list, weights is the prob of occuring  
-#     letter= random.choices(letter_list, weights=[1, 1, 1, 1, 1, 1, 1, 1], k=4)
-
-#     #puts dash (-) in list to separate 
-#     dash_list_let='-'.join(letter)
-
-#     #sort it so the letters dont reproduce 
-
-
-#     #list comphrension, using one list with another 
-#     vowel_count = [i for i in letter if i in vowels]
-#     consonant_count = [i for i in letter if i in consonant]
-#     package = { 
-#         "vowel_count": len(vowel_count), "consonant_count": len(consonant_count) , "dash_list_let": dash_list_let
-#     }
     return jsonify(package) 
 if __name__ == '__main__':
     app.run(host = "0.0.0.0", port = 5002, debug= True )
--- micro-services/service3_numbers/app.py
+++ micro-services/service3_numbers/app.py
@@ -13,26 +13,6 @@ def number_gen():
         random_string += str(random.randint(1,8))
     return random_string
     
-    # even = '2468'
-    # odd = '1357'
-    # #choices let you pick the letters from the lise, k=amount of objects from the list, weights is the prob of occuring  
-    # number= random.choices(number_list, weights=[1, 1, 1, 1, 1, 1, 1, 1], k=4)
-
-    # # print('these are your numbers :')
-    # dash_list_num= '-'.join(number)
-
-    # #list comphrension, using one list with another 
-    # even_count = [i for i in number if i in even]
-    # # print ('you have this many even numbers : ', len(even_count))
-    # odd_count = [i for i in number if i in odd]
-    # # print ('you have this many odd numbers : ', len(odd_count))
-    # #wrapping this into a json package so its extractable for my service 4 
-    # num_package = {
-    #     "even_count": len(even_count), "odd_count":len(odd_count), "dash_list_num": dash_list_num
-    # }
-    # return jsonify(num_package)
-    # # return f"{len(even_count)} {len(odd_count)} {dash_list_num}"
-
 
 
 if __name__ == '__main__':
